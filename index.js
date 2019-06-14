45a9253c32537f19e4f15919a70c48802c1033f3
Stef Spears <spears.sj87@gmail.com>
2019-06-14 18:09:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var sentence = [];
+  for(i = 1; i < musicians.length; i++) {
+    sentence.push(musicians[0] + " plays " + instruments[0]);
+  }
+}
